e3cfef3165464dad194da7807c54f2951869ca7e
bastawy <125200508@sha.edu.eg>
2024-06-24 19:16:34 +0300

    feat(Login) :art: Add login screen and routing for login

--- lib/core/config/routing/app_router.dart
+++ lib/core/config/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../features/auth/login/login_screen.dart';
 import '../../../features/on_boarding/on_boarding_screens.dart';
 import 'no_route_defined_widget.dart';
 import 'routes.dart';
@@ -9,6 +10,8 @@ class AppRouter {
     switch (settings.name) {
       case Routes.onBoardingScreens:
         return MaterialPageRoute(builder: (_) => const OnBoardingScreens());
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
       default:
         return MaterialPageRoute(
           builder: (_) => NoRouteDefinedWidget(settings: settings),
