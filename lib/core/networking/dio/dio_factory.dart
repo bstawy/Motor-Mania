952d3193c74400bd32a8112783c7b6444092d22f
bastawy <125200508@sha.edu.eg>
2024-06-27 19:45:17 +0300

    chore: Update DioFactory to use ApiConstants for base URL and increase timeout duration

--- lib/core/networking/dio/dio_factory.dart
+++ lib/core/networking/dio/dio_factory.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
+import '../constants/api_constants.dart';
 import 'token_interceptor.dart';
 
 class DioFactory {
@@ -11,11 +12,12 @@ class DioFactory {
   static Dio? _tokenDio;
 
   static Dio getFreeDio() {
-    Duration timeOut = const Duration(seconds: 30);
+    Duration timeOut = const Duration(seconds: 60);
 
     if (_freeDio == null) {
       _freeDio = Dio();
       _freeDio!.options
+        ..baseUrl = ApiConstants.baseUrl
         ..connectTimeout = timeOut
         ..receiveTimeout = timeOut;
       _addFreeDioInterceptors();
@@ -26,11 +28,12 @@ class DioFactory {
   }
 
   static Dio getTokenDio() {
-    Duration timeOut = const Duration(seconds: 30);
+    Duration timeOut = const Duration(seconds: 60);
 
     if (_tokenDio == null) {
       _tokenDio = Dio();
       _tokenDio!.options
+        ..baseUrl = ApiConstants.baseUrl
         ..connectTimeout = timeOut
         ..receiveTimeout = timeOut;
       _addTokenDioInterceptors();
