084ec0b72f8a7a9498d23153d08f378ab2f43ca1
bastawy <125200508@sha.edu.eg>
2024-06-27 19:45:52 +0300

    feat: Refactor CRUD manager to support POST requests

--- lib/core/networking/crud_manager.dart
+++ lib/core/networking/crud_manager.dart
@@ -9,13 +9,17 @@ class CrudManager {
     if (_instance == null) {
       _dio = dio;
       _instance = CrudManager._();
+      return _instance;
     } else {
       return _instance;
     }
   }
 
-  Future<T> get<T>(String endPoint,
-      {Map<String, dynamic>? body, dynamic param}) async {
+  Future<T> get<T>(
+    String endPoint, {
+    Map<String, dynamic>? body,
+    dynamic param,
+  }) async {
     try {
       final response = await _dio!.get(
         endPoint,
@@ -28,8 +32,26 @@ class CrudManager {
     }
   }
 
-  Future<void> read() async {
-    // Read a record
+  Future<void> post(
+    String endPoint, {
+    Map<String, dynamic>? body,
+    dynamic param,
+  }) async {
+    try {
+      final response = await _dio!.post(
+        endPoint,
+        data: body,
+        queryParameters: param,
+      );
+
+      if (response.statusCode == 201) {
+        return response.data;
+      } else {
+        throw Exception('Failed to create record');
+      }
+    } catch (e) {
+      rethrow;
+    }
   }
 
   Future<void> update() async {
