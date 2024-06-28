e09abadaa118666eaa515712baa3b6f11083b1d7
bastawy <125200508@sha.edu.eg>
2024-06-28 19:46:01 +0300

    Refactor dependency injection setup for improved code organization and maintainability

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'core/di/dependency_injection.dart';
 import 'motor_mania_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  initGetIt();
 
   runApp(const MotorManiaApp());
 }
