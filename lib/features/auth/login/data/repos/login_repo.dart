90fe89d037a88bac9917f05820e29ac372d8fbbe
bastawy <125200508@sha.edu.eg>
2024-06-30 15:02:20 +0300

    feat: Add LoginRemoteDataSource and LoginRepo classes for login functionality

---
+++ lib/features/auth/login/data/repos/login_repo.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../../core/caching/secure_storage_factory.dart';
+import '../../../../../core/config/constants/caching_constants.dart';
+import '../data_sources/login_remote_data_source.dart';
+import '../models/login_request_body_model.dart';
+
+class LoginRepo {
+  final LoginRemoteDataSource dataSource;
+
+  LoginRepo(this.dataSource);
+
+  Future<Either<String, String>> login(
+      LoginRequestBodyModel requestBody) async {
+    try {
+      final userData = await dataSource.login(requestBody);
+
+      if (userData.accessToken.isNotEmpty && userData.refreshToken.isNotEmpty) {
+        SecureStorageFactory.getInstance().write(
+            key: TokensConstants.accessToken, value: userData.accessToken);
+        SecureStorageFactory.getInstance().write(
+            key: TokensConstants.refreshToken, value: userData.refreshToken);
+
+        return const Right('Login successful');
+      }
+      throw ('Tokens are empty.');
+    } catch (e) {
+      return Left(e.toString());
+    }
+  }
+}
