1267f7288beb880f20d9d9ca0d9cee9a3a1f9e84
bastawy <125200508@sha.edu.eg>
2024-06-30 16:03:10 +0300

    feat: Add RegisterRemoteDataSource class for registration functionality

--- lib/features/auth/register/data/repos/register_repo.dart
+++ lib/features/auth/register/data/repos/register_repo.dart
@@ -1,34 +1,20 @@
 import 'package:dartz/dartz.dart';
 
-import '../../../../../core/config/constants/api_constants.dart';
-import '../../../../../core/networking/crud_manager.dart';
+import '../data_sources/register_remote_data_source.dart';
 import '../models/register_request_body.dart';
-import '../models/register_response.dart';
 
 class RegisterRepo {
-  final CrudManager crudManager;
+  final RegisterRemoteDataSource _remoteDataSource;
+  RegisterRepo(this._remoteDataSource);
 
-  RegisterRepo(this.crudManager);
-
-  Future<Either<List<String>, String>> registerUser(
+  Future<Either<String, String>> registerUser(
       RegisterRequestBodyModel requestBody) async {
     try {
-      final response = await crudManager.post(
-        EndPoints.register,
-        body: requestBody.toJson(),
-      );
+      final response = await _remoteDataSource.register(requestBody);
 
-      if (response.statusCode == 200) {
-        final result = RegisterResponseModel.fromJson(response.data);
-
-        if (result.status == true) {
-          return Right(result.message);
-        }
-        return Left([result.message, result.data.toString()]);
-      }
-      throw Exception('Failed to register user');
+      return Right(response.message);
     } catch (e) {
-      rethrow;
+      return Left(e.toString());
     }
   }
 }
