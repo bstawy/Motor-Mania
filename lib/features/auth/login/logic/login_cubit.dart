e2c063862a09df609cfff1dccebe081bcce487be
bastawy <125200508@sha.edu.eg>
2024-06-30 15:02:29 +0300

    feat: Implement LoginCubit for login functionality

---
+++ lib/features/auth/login/logic/login_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../data/models/login_request_body_model.dart';
+import '../data/repos/login_repo.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final LoginRepo loginRepo;
+  LoginCubit(this.loginRepo) : super(InitialState());
+
+  Future<void> login(LoginRequestBodyModel requestBody) async {
+    emit(LoadingState());
+    try {
+      final response = await loginRepo.login(requestBody);
+      response.fold(
+        (l) => emit(ErrorState(l)),
+        (r) => emit(SuccessState(r)),
+      );
+    } catch (e) {
+      emit(ErrorState(e.toString()));
+    }
+  }
+}
