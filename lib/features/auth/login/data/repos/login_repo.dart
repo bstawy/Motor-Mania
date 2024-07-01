15097c42b3cea5ce6e0b94023585b7948b90b36d
bastawy <125200508@sha.edu.eg>
2024-07-01 21:00:12 +0300

    chore: Update login repository to handle login response using Either type

--- lib/features/auth/login/data/repos/login_repo.dart
+++ lib/features/auth/login/data/repos/login_repo.dart
@@ -11,18 +11,22 @@ class LoginRepo {
 
   Future<Either<String, String>> login(
       LoginRequestBodyModel requestBody) async {
-    try {
-      final userData = await _dataSource.login(requestBody);
+    final response = await _dataSource.login(requestBody);
 
-      if (userData.accessToken.isNotEmpty && userData.refreshToken.isNotEmpty) {
-        TokensManager.setAccessToken(userData.accessToken);
-        TokensManager.setRefreshToken(userData.refreshToken);
+    return response.fold(
+      (l) {
+        return Left(l);
+      },
+      (userData) {
+        if (userData.accessToken.isNotEmpty &&
+            userData.refreshToken.isNotEmpty) {
+          TokensManager.setAccessToken(userData.accessToken);
+          TokensManager.setRefreshToken(userData.refreshToken);
 
-        return const Right('Login successful');
-      }
-      throw ('Tokens are empty.');
-    } catch (e) {
-      return Left(e.toString());
-    }
+          return const Right('Login successful');
+        }
+        return const Left('Tokens are empty.');
+      },
+    );
   }
 }
