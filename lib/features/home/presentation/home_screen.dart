a928fc419db45f4f7dd1d4c953e340c4e8510e1d
bastawy <125200508@sha.edu.eg>
2024-07-04 19:25:49 +0300

    feat: Add lazy loading for images on home page

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
 
+import 'widgets/categories_filter_widget.dart';
 import 'widgets/home_header.dart';
 import 'widgets/search_bar_widget.dart';
 
@@ -12,9 +14,12 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          const Column(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              HomeHeaderWidget(),
+              const HomeHeaderWidget(),
+              Gap(40.h),
+              const CategoriesFilterWidget()
             ],
           ),
           Positioned(
