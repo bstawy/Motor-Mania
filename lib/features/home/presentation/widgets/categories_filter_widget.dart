90c294da2ba9f7bfe4b0691a81c18b803b8c5e46
bastawy <125200508@sha.edu.eg>
2024-07-05 20:28:15 +0300

    feat: Add electric and suspension category icons

--- lib/features/home/presentation/widgets/categories_filter_widget.dart
+++ lib/features/home/presentation/widgets/categories_filter_widget.dart
@@ -6,15 +6,24 @@ import '../../../../core/config/text/text_styles.dart';
 import '../../../../core/helpers/extensions/extensions.dart';
 import 'custom_filter_chip_widget.dart';
 
-class CategoriesFilterWidget extends StatelessWidget {
-  CategoriesFilterWidget({super.key});
+class CategoriesFilterWidget extends StatefulWidget {
+  const CategoriesFilterWidget({super.key});
+
+  @override
+  State<CategoriesFilterWidget> createState() => _CategoriesFilterWidgetState();
+}
+
+class _CategoriesFilterWidgetState extends State<CategoriesFilterWidget> {
+  int selectedCategoryIndex = 0;
 
   final List<String> categories = [
-    "Mechanic",
-    "Electrician",
-    "Plumber",
-    "Carpenter",
-    "Painter",
+    "Engine",
+    "Brakes",
+    "Exterior",
+    "Electric",
+    "Tyres",
+    "Suspension",
+    "Interior",
   ];
 
   @override
@@ -24,20 +33,31 @@ class CategoriesFilterWidget extends StatelessWidget {
       children: [
         Text(
           "Categories",
-          style: TextStyles.font20DarkBlueBold,
+          style: TextStyles.font14DarkBlueSemiBold,
         ).setHorizontalPadding(16.w),
         Gap(8.h),
         SizedBox(
-          height: 40.h,
+          height: 64.h,
           child: ListView.builder(
             padding: EdgeInsets.symmetric(horizontal: 16.w),
             itemCount: categories.length,
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) {
-              return CustomFilterChip(
-                title: categories[index],
-                isSelected: index == 0,
-              ).setOnlyPadding(0, 0, 4.w, 0);
+              return GestureDetector(
+                onTap: () {
+                  if (selectedCategoryIndex != index) {
+                    setState(() {
+                      selectedCategoryIndex = index;
+                    });
+                  }
+                },
+                child: CustomFilterChip(
+                  title: categories[index],
+                  imagePath:
+                      "assets/icons/category_${categories[index].toLowerCase()}_icon.svg",
+                  isSelected: selectedCategoryIndex == index,
+                ).setOnlyPadding(0, 0, 8.w, 0),
+              );
             },
           ),
         ),
--- lib/features/home/presentation/widgets/custom_filter_chip_widget.dart
+++ lib/features/home/presentation/widgets/custom_filter_chip_widget.dart
@@ -1,39 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gap/gap.dart';
 
 import '../../../../core/config/text/text_styles.dart';
 import '../../../../core/config/theme/colors_manager.dart';
 
 class CustomFilterChip extends StatelessWidget {
   final String title;
+  final String imagePath;
   final bool isSelected;
 
   const CustomFilterChip({
     super.key,
     required this.title,
+    required this.imagePath,
     required this.isSelected,
   });
 
   @override
   Widget build(BuildContext context) {
-    return FilterChip(
-      label: Text(
-        title,
-        style: TextStyles.font10WhiteSemiBold.copyWith(
-          color: isSelected ? Colors.white : ColorsManager.darkkBlue,
-        ),
+    return Container(
+      padding: EdgeInsets.all(12.r),
+      decoration: BoxDecoration(
+        color: isSelected
+            ? ColorsManager.darkkBlue.withOpacity(0.7)
+            : Colors.white,
+        borderRadius: BorderRadius.circular(15.r),
       ),
-      onSelected: (value) {},
-      selected: isSelected,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(70.r),
-      ),
-      selectedColor: ColorsManager.darkkBlue,
-      elevation: 0,
-      showCheckmark: false,
-      padding: EdgeInsets.symmetric(
-        horizontal: 16.w,
-        vertical: 8.h,
+      child: Column(
+        children: [
+          SvgPicture.asset(imagePath),
+          Gap(2.h),
+          Text(
+            title,
+            style: TextStyles.font10WhiteSemiBold.copyWith(
+              color: isSelected ? Colors.white : ColorsManager.darkkBlue,
+            ),
+          ),
+        ],
       ),
     );
   }
