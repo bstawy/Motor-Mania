eb93ef47f5453410e30a2d9b3036db0aa1d9a48d
bastawy <125200508@sha.edu.eg>
2024-07-05 18:50:07 +0300

    Refactor home screen layout for improved performance and responsiveness

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -15,30 +15,32 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const HomeHeaderWidget(),
-              Gap(40.h),
-              CategoriesFilterWidget(),
-              Gap(24.h),
-              Text(
-                "Best Sellers",
-                style: TextStyles.font20DarkBlueBold,
-              ).setHorizontalPadding(16.w),
-              Gap(8.h),
-              Expanded(child: BestSellerGridWidget()),
-            ],
-          ),
-          Positioned(
-            top: 278.h,
-            left: 0,
-            right: 0,
-            child: const SearchBarWidget(),
-          ),
-        ],
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const HomeHeaderWidget(),
+                Gap(40.h),
+                CategoriesFilterWidget(),
+                Gap(24.h),
+                Text(
+                  "Best Sellers",
+                  style: TextStyles.font20DarkBlueBold,
+                ).setHorizontalPadding(16.w),
+                Gap(8.h),
+                BestSellerGridWidget(),
+              ],
+            ),
+            Positioned(
+              top: 278.h,
+              left: 0,
+              right: 0,
+              child: const SearchBarWidget(),
+            ),
+          ],
+        ),
       ),
     );
   }
