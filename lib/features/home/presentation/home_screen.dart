268fd4660e5c96fdeb8ba9d47822d91ecf3910d2
bastawy <125200508@sha.edu.eg>
2024-07-06 03:25:43 +0300

    feat: Add ColorfulSafeArea to improve UI consistency and handle system overlays

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,13 +1,15 @@
+import 'package:colorful_safe_area/colorful_safe_area.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 
-import '../../../core/config/text/text_styles.dart';
-import '../../../core/helpers/extensions/extensions.dart';
-import 'widgets/best_seller_grid_view.dart';
-import 'widgets/categories_filter_widget.dart';
+import '../../../core/config/theme/colors_manager.dart';
+import 'widgets/best_sellers_widget.dart';
+import 'widgets/categories/categories_widget.dart';
 import 'widgets/home_header.dart';
-import 'widgets/search_bar_widget.dart';
+import 'widgets/offers_widget.dart';
+import 'widgets/recommended_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -15,31 +17,34 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: ColorfulSafeArea(
+          color: ColorsManager.darkkBlue,
+          child: SingleChildScrollView(
+            child: Stack(
               children: [
-                const HomeHeaderWidget(),
-                Gap(40.h),
-                CategoriesFilterWidget(),
-                Gap(24.h),
-                Text(
-                  "Best Sellers",
-                  style: TextStyles.font20DarkBlueBold,
-                ).setHorizontalPadding(16.w),
-                Gap(8.h),
-                BestSellerGridWidget(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const HomeHeaderWidget(),
+                    Gap(95.h),
+                    const CategoriesWidget(),
+                    Gap(24.h),
+                    RecommendedWidget(),
+                    Gap(16.h),
+                    BestSellersWidget(),
+                  ],
+                ),
+                Positioned(
+                  top: 285.h,
+                  left: 0,
+                  right: 0,
+                  child: const OffersWidget(),
+                ),
               ],
             ),
-            Positioned(
-              top: 278.h,
-              left: 0,
-              right: 0,
-              child: const SearchBarWidget(),
-            ),
-          ],
+          ),
         ),
       ),
     );
