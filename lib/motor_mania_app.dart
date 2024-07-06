e088b7e73eabb61256e6c08e040c0c4d850eedbf
bastawy <125200508@sha.edu.eg>
2024-07-06 18:25:09 +0300

    refactor: Update main.dart and motor_mania_app.dart to pass token as a parameter

--- lib/motor_mania_app.dart
+++ lib/motor_mania_app.dart
@@ -1,62 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'core/caching/tokens_manager.dart';
 import 'core/config/routing/app_router.dart';
 import 'core/config/routing/routes.dart';
 import 'core/config/theme/app_theme.dart';
 
-class MotorManiaApp extends StatefulWidget {
-  const MotorManiaApp({super.key});
-
-  @override
-  State<MotorManiaApp> createState() => _MotorManiaAppState();
-}
-
-class _MotorManiaAppState extends State<MotorManiaApp> {
-  String? token;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      getToken();
-    });
-  }
-
-  void getToken() async {
-    token = await TokensManager.getAccessToken();
-    setState(() {});
-  }
+class MotorManiaApp extends StatelessWidget {
+  final String token;
+  const MotorManiaApp({super.key, required this.token});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: TokensManager.getAccessToken(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (snapshot.hasError) {
-          return Center(
-            child: Text(snapshot.error.toString()),
-          );
-        }
-        token = snapshot.data;
-
-        return ScreenUtilInit(
-          designSize: const Size(375, 812),
-          child: MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'Motor Mania',
-            theme: AppTheme.lightThemeData,
-            onGenerateRoute: AppRouter().generateRoute,
-            initialRoute:
-                token != null ? Routes.layoutScreen : Routes.onBoardingScreens,
-          ),
-        );
-      },
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Motor Mania',
+        theme: AppTheme.lightThemeData,
+        onGenerateRoute: AppRouter().generateRoute,
+        initialRoute:
+            token.isNotEmpty ? Routes.layoutScreen : Routes.onBoardingScreens,
+      ),
     );
   }
 }
