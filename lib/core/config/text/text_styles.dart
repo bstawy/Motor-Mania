3f5d457ee6b7a0a81bdbbde8c0dde271da0c0328
bastawy <125200508@sha.edu.eg>
2024-07-06 03:24:14 +0300

    chore: Update text styles and colors

--- lib/core/config/text/text_styles.dart
+++ lib/core/config/text/text_styles.dart
@@ -131,6 +131,13 @@ class TextStyles {
     fontFamily: 'Montserrat',
   );
 
+  static TextStyle font10DarkBlueMedium = TextStyle(
+    color: ColorsManager.darkkBlue,
+    fontSize: 10.sp,
+    fontWeight: FontWeightHelper.medium,
+    fontFamily: 'Montserrat',
+  );
+
   static TextStyle font10WhiteMedium = TextStyle(
     color: Colors.white,
     fontSize: 10.sp,
@@ -140,6 +147,13 @@ class TextStyles {
 
   static TextStyle font10DarkBlueRegular = TextStyle(
     color: ColorsManager.darkkBlue,
+    fontSize: 10.sp,
+    fontWeight: FontWeightHelper.regular,
+    fontFamily: 'Montserrat',
+  );
+
+  static TextStyle font10LightGreyRegular = TextStyle(
+    color: ColorsManager.lightGrey,
     fontSize: 10.sp,
     fontWeight: FontWeightHelper.regular,
     fontFamily: 'Montserrat',
