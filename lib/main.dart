609176d84f64ebbe03a89911530612f58611b454
bastawy <125200508@sha.edu.eg>
2024-07-08 17:24:42 +0300

    refactor: Update AppManagerCubit to check user login status and emit corresponding state

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'core/config/app_manager/app_manager_cubit.dart';
 import 'core/di/dependency_injection.dart';
 import 'motor_mania_app.dart';
 
@@ -9,5 +11,10 @@ void main() async {
   await ScreenUtil.ensureScreenSize();
   initGetIt();
 
-  runApp(const MotorManiaApp());
+  runApp(
+    BlocProvider<AppManagerCubit>(
+      create: (context) => AppManagerCubit(),
+      child: const MotorManiaApp(),
+    ),
+  );
 }
