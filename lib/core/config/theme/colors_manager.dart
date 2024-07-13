63e239c38ca76f4f2d4188ea8613beb3d2c9fb22
bastawy <125200508@sha.edu.eg>
2024-07-13 21:46:51 +0300

    chore: Update font styles and colors

--- lib/core/config/theme/colors_manager.dart
+++ lib/core/config/theme/colors_manager.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class ColorsManager {
   static const Color darkkBlue = Color(0xFF171823);
+  static const Color lighterBlue = Color(0xFF8D99AF);
   static const Color grey = Color(0xFF828A89);
   static const Color lightGrey = Color(0xFF8D99AF);
   static const Color lighterGrey = Color(0xFFF9F9F9);
