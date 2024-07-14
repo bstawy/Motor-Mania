cc92226ef2e2c00cec193c5d732dfbf1a6a23a1a
bastawy <125200508@sha.edu.eg>
2024-07-14 17:30:42 +0300

    refactor: Update pushNamed and pushReplacementNamed methods in navigation_ext.dart push a new screen above the whole PersistentTabView, ultimately hiding your navigation bar

--- lib/core/helpers/extensions/navigation_ext.dart
+++ lib/core/helpers/extensions/navigation_ext.dart
@@ -2,22 +2,23 @@ import 'package:flutter/widgets.dart';
 
 extension Navigation on BuildContext {
   Future<dynamic> pushNamed(String routeName, {Object? arguments}) {
-    return Navigator.of(this).pushNamed(routeName, arguments: arguments);
+    return Navigator.of(this, rootNavigator: true)
+        .pushNamed(routeName, arguments: arguments);
   }
 
   Future<dynamic> pushReplacementNamed(String routeName, {Object? arguments}) {
-    return Navigator.of(this)
+    return Navigator.of(this, rootNavigator: true)
         .pushReplacementNamed(routeName, arguments: arguments);
   }
 
   Future<dynamic> pushNamedAndRemoveUntil(String routeName,
       {Object? arguments, required RoutePredicate predicate}) {
-    return Navigator.of(this)
+    return Navigator.of(this, rootNavigator: true)
         .pushNamedAndRemoveUntil(routeName, predicate, arguments: arguments);
   }
 
-  void pop() => Navigator.of(this).pop();
+  void pop() => Navigator.of(this, rootNavigator: true).pop();
 
-  void popUntil(String routeName) =>
-      Navigator.of(this).popUntil(ModalRoute.withName(routeName));
+  void popUntil(String routeName) => Navigator.of(this, rootNavigator: true)
+      .popUntil(ModalRoute.withName(routeName));
 }
