d33298000079e645da3e04500b1d662f2c94751b
bastawy <125200508@sha.edu.eg>
2024-07-14 16:19:13 +0300

    feat: Add logic to store user tokens after successful registration

--- lib/features/auth/register/data/repos/register_repo.dart
+++ lib/features/auth/register/data/repos/register_repo.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 
+import '../../../../../core/caching/tokens_manager.dart';
 import '../data_sources/register_remote_data_source.dart';
 import '../models/register_request_body.dart';
 
@@ -11,6 +12,19 @@ class RegisterRepo {
       RegisterRequestBodyModel requestBody) async {
     final response = await _remoteDataSource.register(requestBody);
 
-    return response.fold((l) => Left(l), (r) => Right(r.message));
+    return response.fold(
+      (l) => Left(l),
+      (userData) {
+        final userTokens = userData.tokens;
+        if (userTokens.accessToken.isNotEmpty &&
+            userTokens.refreshToken.isNotEmpty) {
+          TokensManager.setAccessToken(userTokens.accessToken);
+          TokensManager.setRefreshToken(userTokens.refreshToken);
+
+          return const Right('Registered successfully');
+        }
+        return const Left('Tokens are empty.');
+      },
+    );
   }
 }
