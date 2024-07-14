984f553eb696891604697d0f8078afa13990f7fc
bastawy <125200508@sha.edu.eg>
2024-07-14 16:18:35 +0300

    refactor: Remove unnecessary verifyCode field in RegisterRequestBodyModel

--- lib/features/auth/register/data/models/register_request_body.dart
+++ lib/features/auth/register/data/models/register_request_body.dart
@@ -2,13 +2,11 @@ class RegisterRequestBodyModel {
   final String name;
   final String email;
   final String password;
-  final String verifyCode;
 
   RegisterRequestBodyModel({
     required this.name,
     required this.email,
     required this.password,
-    this.verifyCode = "123456",
   });
 
   Map<String, dynamic> toJson() {
@@ -16,7 +14,6 @@ class RegisterRequestBodyModel {
       'name': name,
       'email': email,
       'password': password,
-      'verifyCode': verifyCode,
     };
   }
 }
