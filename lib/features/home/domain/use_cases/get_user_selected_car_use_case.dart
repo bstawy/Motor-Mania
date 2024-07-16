19dafe7dd76ff2bc4ee2da3697ed629d83f7c164
bastawy <125200508@sha.edu.eg>
2024-07-16 15:23:01 +0300

    refactor: Remove unused data sources and models in the home feature

--- lib/features/home/domain/use_cases/get_user_selected_car_use_case.dart
+++ lib/features/home/domain/use_cases/get_user_selected_car_use_case.dart
@@ -1,14 +1,20 @@
 import 'package:dartz/dartz.dart';
 
+import '../../../../core/networking/failure/failures.dart';
 import '../entities/car_entity.dart';
-import '../repos/user_selected_car_repo.dart';
+import '../repos/home_repo.dart';
 
 class GetUserSelectedCarUseCase {
-  final UserSelectedCarRepo _userSelectedCarRepo;
+  final HomeRepo _homeRepo;
 
-  GetUserSelectedCarUseCase(this._userSelectedCarRepo);
+  GetUserSelectedCarUseCase(this._homeRepo);
 
-  Future<Either<String, CarEntity>> exeute() async {
-    return _userSelectedCarRepo.getUserSelectedCar();
+  Future<Either<ServerFailure, CarEntity>> execute() async {
+    final response = await _homeRepo.getUserCar();
+
+    return response.fold(
+      (failure) => Left(failure),
+      (carModel) => Right(carModel.toEntity(50000)),
+    );
   }
 }
