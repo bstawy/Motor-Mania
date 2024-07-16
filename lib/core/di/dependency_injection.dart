7977755c1afa1156c20027a9ee14779b1e156dd4
bastawy <125200508@sha.edu.eg>
2024-07-16 15:23:18 +0300

    refactor: Update dependency injection for home feature

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -7,22 +7,12 @@ import '../../features/auth/login/logic/login_cubit.dart';
 import '../../features/auth/register/data/data_sources/register_remote_data_source.dart';
 import '../../features/auth/register/data/repos/register_repo.dart';
 import '../../features/auth/register/logic/register_cubit.dart';
-import '../../features/home/data/data_sources/categories_remote_data_source.dart';
-import '../../features/home/data/data_sources/data_sources_impl/categories_remote_data_source_impl.dart';
-import '../../features/home/data/data_sources/data_sources_impl/products_remote_data_source_impl.dart';
-import '../../features/home/data/data_sources/data_sources_impl/user_selected_car_remote_data_source_impl.dart';
-import '../../features/home/data/data_sources/products_remote_data_source.dart';
-import '../../features/home/data/data_sources/user_selected_car_remote_data_source.dart';
-import '../../features/home/data/repos_impl/categories_repo_impl.dart';
-import '../../features/home/data/repos_impl/products_repo_impl.dart';
-import '../../features/home/data/repos_impl/user_selected_car_repo_impl.dart';
-import '../../features/home/domain/repos/categories_repo.dart';
-import '../../features/home/domain/repos/products_repo.dart';
-import '../../features/home/domain/repos/user_selected_car_repo.dart';
-import '../../features/home/domain/use_cases/get_all_categories_use_case.dart';
-import '../../features/home/domain/use_cases/get_all_products_use_case.dart';
-import '../../features/home/domain/use_cases/get_category_products_use_case.dart';
-import '../../features/home/domain/use_cases/get_prodcut_details_use_case.dart';
+import '../../features/home/data/data_sources/home_remote_data_source.dart';
+import '../../features/home/data/data_sources_impl/home_remote_data_source_impl.dart';
+import '../../features/home/data/repos_impl/home_repo_impl.dart';
+import '../../features/home/domain/repos/home_repo.dart';
+import '../../features/home/domain/use_cases/get_home_categories_use_case.dart';
+import '../../features/home/domain/use_cases/get_home_products_use_case.dart';
 import '../../features/home/domain/use_cases/get_user_selected_car_use_case.dart';
 import '../../features/home/presentation/logic/home_cubit.dart';
 import '../networking/crud_manager.dart';
@@ -53,32 +43,26 @@ Future<void> initGetIt() async {
   getIt.registerFactory<LoginCubit>(() => LoginCubit(getIt()));
 
   // home
-  getIt.registerFactory<CategoriesRemoteDataSource>(
-      () => CategoriesRemoteDataSourceImpl(getIt()));
-  getIt.registerFactory<CategoriesRepo>(() => CategoriesRepoImpl(getIt()));
-  getIt.registerFactory<GetAllCategoriesUseCase>(
-      () => GetAllCategoriesUseCase(getIt()));
-
-  getIt.registerFactory<ProductsRemoteDataSource>(
-      () => ProductsRemoteDataSourceImpl(getIt()));
-  getIt.registerFactory<ProductsRepo>(() => ProductsRepoImpl(getIt()));
-  getIt.registerFactory<GetAllProductsUseCase>(
-      () => GetAllProductsUseCase(getIt()));
-
-  getIt.registerFactory<UserSelectedCarRemoteDataSource>(
-      () => UserSelectedCarRemoteDataSourceImpl(getIt()));
-  getIt.registerFactory<UserSelectedCarRepo>(
-      () => UserSelectedCarRepoImpl(getIt()));
+  getIt.registerFactory<HomeRemoteDataSource>(
+    () => HomeRemoteDataSourceImpl(getIt()),
+  );
+  getIt.registerFactory<HomeRepo>(
+    () => HomeRepoImpl(getIt()),
+  );
   getIt.registerFactory<GetUserSelectedCarUseCase>(
-      () => GetUserSelectedCarUseCase(getIt()));
-
-  getIt.registerFactory<GetProdcutDetailsUseCase>(
-      () => GetProdcutDetailsUseCase(getIt()));
-
+    () => GetUserSelectedCarUseCase(getIt()),
+  );
+  getIt.registerFactory<GetHomeCategoriesUseCase>(
+    () => GetHomeCategoriesUseCase(getIt()),
+  );
+  getIt.registerFactory<GetHomeProductsUseCase>(
+    () => GetHomeProductsUseCase(getIt()),
+  );
   getIt.registerFactory<HomeCubit>(
-      () => HomeCubit(getIt(), getIt(), getIt(), getIt(), getIt()));
-
-  // CategoryProducts
-  getIt.registerFactory<GetCategoryProductsUseCase>(
-      () => GetCategoryProductsUseCase(getIt()));
+    () => HomeCubit(
+      getIt(),
+      getIt(),
+      getIt(),
+    ),
+  );
 }
