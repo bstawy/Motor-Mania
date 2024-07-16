a05b72e94ca1b8264c92ef5a1490ca5a25bec23b
bastawy <125200508@sha.edu.eg>
2024-07-16 15:22:44 +0300

    refactor: Remove unused data sources and models in the home feature

---
+++ lib/features/home/data/repos_impl/home_repo_impl.dart
@@ -0,0 +1,108 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:motor_mania/features/home/data/models/home_car_model.dart';
+
+import '../../../../core/networking/failure/failures.dart';
+import '../../domain/entities/category_entity.dart';
+import '../../domain/repos/home_repo.dart';
+import '../data_sources/home_remote_data_source.dart';
+import '../models/home_category_model.dart';
+import '../models/home_product_model.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource _homeRemoteDataSource;
+
+  HomeRepoImpl(this._homeRemoteDataSource);
+  @override
+  Future<Either<ServerFailure, CarModel>> getUserCar() async {
+    try {
+      final response = await _homeRemoteDataSource.getUserCar();
+
+      if (response.statusCode == 200 && response.data['success'] == true) {
+        debugPrint("============================");
+
+        final CarModel userCar = CarModel.fromJson(response.data['data']);
+
+        return Right(userCar);
+      }
+
+      return Left(
+        ServerFailure(
+          statusCode: response.statusCode.toString(),
+          message: response.data['message'],
+        ),
+      );
+    } catch (e) {
+      return Left(
+        ServerFailure(
+          statusCode: '400',
+          message: e.toString(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<ServerFailure, List<HomeCategoryEntity>>>
+      getHomeCategories() async {
+    try {
+      final response = await _homeRemoteDataSource.getHomeCategories();
+
+      if (response.statusCode == 200 && response.data['success'] == true) {
+        List<HomeCategoryEntity> homeCategories =
+            (response.data['data'] as List)
+                .map((i) => HomeCategoryModel.fromJson(i))
+                .toList();
+
+        return Right(homeCategories);
+      }
+
+      return Left(
+        ServerFailure(
+          statusCode: response.statusCode.toString(),
+          message: response.data['message'],
+        ),
+      );
+    } catch (e) {
+      return Left(
+        ServerFailure(
+          statusCode: '400',
+          message: e.toString(),
+        ),
+      );
+    }
+  }
+
+  @override
+  Future<Either<ServerFailure, List<HomeProductModel>>>
+      getHomeProducts() async {
+    try {
+      final response = await _homeRemoteDataSource.getHomeProducts();
+
+      if (response.statusCode == 200 && response.data['success'] == true) {
+        final List<HomeProductModel> homeProducts =
+            (response.data['data'] as List)
+                .map((i) => HomeProductModel.fromJson(i))
+                .toList();
+
+        return Right(homeProducts);
+      }
+      debugPrint("============================");
+      debugPrint(response.data['status'].toString());
+
+      return Left(
+        ServerFailure(
+          statusCode: response.statusCode.toString(),
+          message: response.data['message'],
+        ),
+      );
+    } catch (e) {
+      return Left(
+        ServerFailure(
+          statusCode: '400',
+          message: e.toString(),
+        ),
+      );
+    }
+  }
+}
--- lib/features/home/domain/repos/products_repo.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-import '../entities/product_entity.dart';
-
-abstract class ProductsRepo {
-  Future<Either<String, List<ProductEntity>>> getProducts();
-
-  Future<Either<String, ProductEntity>> getProductDetails(int id);
-}
