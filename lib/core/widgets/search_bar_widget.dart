3484974c980f0546bf5ed5b86bd5b72588eb9983
bastawy <125200508@sha.edu.eg>
2024-07-21 22:28:35 +0300

    feat: Add success and error snack bar helpers to context

--- lib/core/widgets/search_bar_widget.dart
+++ lib/core/widgets/search_bar_widget.dart
@@ -5,6 +5,7 @@ import 'package:gap/gap.dart';
 
 import '../config/text/text_styles.dart';
 import '../config/theme/colors_manager.dart';
+import '../helpers/extensions/snackbar_ext.dart';
 
 class SearchBarWidget extends StatelessWidget {
   final String? hintText;
@@ -36,10 +37,7 @@ class SearchBarWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        onTap ??
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text("Search Bar")),
-            );
+        onTap ?? context.successSnackBar("Search Bar");
       },
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 8.h),
@@ -68,10 +66,7 @@ class SearchBarWidget extends StatelessWidget {
             const Spacer(),
             InkWell(
               onTap: () {
-                onSuffixIconTap ??
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Barcode")),
-                    );
+                onSuffixIconTap ?? context.successSnackBar("Barcode");
               },
               child: suffixIcon ??
                   SvgPicture.asset(
