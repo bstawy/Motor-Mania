12ef5984ff39f965b4d49759813654053a0eb317
bastawy <125200508@sha.edu.eg>
2024-07-27 22:37:54 +0300

    refactor: Remove unused import of shimmer package in categories_list_widget.dart

--- lib/features/home/presentation/ui/widgets/categories_list_widget.dart
+++ lib/features/home/presentation/ui/widgets/categories_list_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
-import 'package:shimmer/shimmer.dart';
 
 import '../../../../../core/config/text/text_styles.dart';
 import '../../../../../core/helpers/extensions/extensions.dart';
