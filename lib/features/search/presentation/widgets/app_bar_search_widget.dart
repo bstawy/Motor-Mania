014012b363c968a666d07b0ab0cf7e09f16415bf
bastawy <125200508@sha.edu.eg>
2024-07-27 22:38:09 +0300

    feat: Add Debouncer class for handling delayed actions
    - Added Debouncer class to handle delayed actions in the application
    - The Debouncer class allows for debouncing actions to avoid multiple requests or executions
    - Used Debouncer class in AppBarSearchWidget to debounce search query changes
    - Removed unused import of Timer class in AppBarSearchWidget

--- lib/features/search/presentation/widgets/app_bar_search_widget.dart
+++ lib/features/search/presentation/widgets/app_bar_search_widget.dart
@@ -1,11 +1,10 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../core/config/text/text_styles.dart';
 import '../../../../core/config/theme/colors_manager.dart';
+import '../../../../core/helpers/debouncer.dart';
 import '../../../../core/helpers/extensions/extensions.dart';
 import '../logic/search_cubit.dart';
 
@@ -18,7 +17,7 @@ class AppBarSearchWidget extends StatefulWidget {
 
 class _AppBarSearchWidgetState extends State<AppBarSearchWidget> {
   late TextEditingController _searchController;
-  Timer? _debounceTimer;
+  final Debouncer _debouncer = Debouncer(milliseconds: 300);
   String lastQuery = "";
 
   @override
@@ -30,11 +29,7 @@ class _AppBarSearchWidgetState extends State<AppBarSearchWidget> {
   // debounce search query to avoid multiple requests
   // send search request only when user stop typing
   void _onSearchQueryChanged(String query) {
-    if (_debounceTimer?.isActive ?? false) {
-      _debounceTimer?.cancel();
-    }
-
-    _debounceTimer = Timer(const Duration(milliseconds: 300), () {
+    _debouncer.run(() {
       if (query != lastQuery) {
         lastQuery = query;
         context.read<SearchCubit>().search(query);
@@ -44,7 +39,6 @@ class _AppBarSearchWidgetState extends State<AppBarSearchWidget> {
 
   @override
   void dispose() {
-    _debounceTimer?.cancel();
     _searchController.dispose();
     super.dispose();
   }
