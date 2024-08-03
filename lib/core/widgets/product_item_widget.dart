5cee6a7f576ace4129f89dab6ee7c1968920e787
bastawy <125200508@sha.edu.eg>
2024-08-04 01:30:32 +0300

    refactor: Update ProductItemWidget to include optional parameters for card width, image height, and show discount

---
+++ lib/core/widgets/product_item_widget.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:gap/gap.dart';
+
+import '../../features/home/domain/entities/home_product_entity.dart';
+import '../../features/product_details/presentation/ui/widgets/product_price_and_discount_widget.dart';
+import '../config/text/text_styles.dart';
+import '../config/theme/colors_manager.dart';
+import '../helpers/extensions/extensions.dart';
+import '../helpers/open_product_bottom_sheet.dart';
+import 'cart_button_widget.dart';
+import 'favorite_button_widget.dart';
+import 'product_name_and_type_widget.dart';
+import 'product_property_widget.dart';
+
+class ProductItemWidget extends StatelessWidget {
+  final HomeProductEntity product;
+  final double? cardWidth;
+  final double? imageHeight;
+  final bool? showDiscount;
+
+  const ProductItemWidget({
+    super.key,
+    required this.product,
+    this.cardWidth,
+    this.imageHeight,
+    this.showDiscount = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        openProductBottomSheet(context: context, productId: product.id ?? "");
+      },
+      child: Container(
+        width: cardWidth ?? 150.w,
+        padding: EdgeInsets.all(8.r),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15.r),
+        ),
+        child: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Gap(28.h),
+                SizedBox(
+                  height: imageHeight ?? 88.h,
+                  child: Image.network(product.imageUrl ?? ""),
+                ),
+                Gap(12.h),
+                ProductPropertyWidget(
+                  title: "${product.rating} (${product.reviewsCount})",
+                  iconPath: "assets/icons/star_icon.svg",
+                  titleStyle: TextStyles.font8BlueGreyMedium,
+                ),
+                ProductNameAndTypeWidget(
+                  name: product.name ?? "",
+                  type: product.compatibleCars?.first.brand ?? "",
+                ),
+                Gap(6.h),
+                ProductPriceAndDiscountWidget(
+                  price: product.price ?? 0,
+                  firstGap: 6.w,
+                  oldPrice: product.oldPrice ?? 0,
+                  priceStyle: TextStyles.font14DarkBlueBold,
+                  oldPriceStyle: TextStyles.font10LightGreyRegular,
+                  discountPercentage: showDiscount ?? true
+                      ? product.discountPercentage ?? 0
+                      : null,
+                  discountPercentageStyle: TextStyles.font7RedSemiBold,
+                ),
+                Gap(5.h),
+                ProductPropertyWidget(
+                  title: product.freeDelivery ?? true
+                      ? "Free Delivery"
+                      : "Verified Seller",
+                  iconPath: product.freeDelivery ?? true
+                      ? "assets/icons/free_delivery_filled_icon.svg"
+                      : "assets/icons/verify_icon.svg",
+                ),
+                Gap(8.h),
+                CartButtonWidget(product: product),
+              ],
+            ).setHorizontalPadding(8.w),
+            Positioned(
+              right: 0,
+              child: FavoriteButtonWidget(product: product),
+            ),
+            Visibility(
+              visible: product.newProduct ?? false || product.amount! < 3,
+              child: Positioned(
+                left: 0,
+                child: Container(
+                  padding: EdgeInsets.all(5.r),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.r),
+                    color: product.newProduct ?? false
+                        ? ColorsManager.red
+                        : ColorsManager.darkkBlue,
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    product.newProduct ?? false
+                        ? "NEW"
+                        : "only ${product.amount} left",
+                    style: TextStyles.font7WhiteMedium,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
