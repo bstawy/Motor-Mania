2b4a21c60918e384d0703cc39e8e841a352d7403
bastawy <125200508@sha.edu.eg>
2024-08-20 18:22:16 +0300

    feat: Add CustomNetworkImage widget for displaying network images

---
+++ lib/core/widgets/custom_network_image_widget.dart
@@ -0,0 +1,34 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+import '../config/theme/colors_manager.dart';
+
+class CustomNetworkImage extends StatelessWidget {
+  final String url;
+  final double? imageHeight, imageWidth;
+
+  const CustomNetworkImage({
+    super.key,
+    required this.url,
+    this.imageHeight,
+    this.imageWidth,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: url,
+      placeholder: (context, url) => const Center(
+        child: CircularProgressIndicator(
+          color: ColorsManager.darkBlue,
+        ),
+      ),
+      errorWidget: (context, url, error) => const Icon(
+        Icons.error,
+        color: ColorsManager.red,
+      ),
+      height: imageHeight,
+      width: imageWidth,
+    );
+  }
+}
